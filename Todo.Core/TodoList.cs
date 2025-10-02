23eb33624da4430522c5aa1db633684cfaf34ecf
TheComing06 <t.abdrahmanov999@gmail.com>
2025-10-03 02:05:30 +0700

    Initial files

--- Todo.Core/TodoList.cs
+++ Todo.Core/TodoList.cs
@@ -2,25 +2,6 @@
 
 namespace Todo.Core
 {
-    public class TodoItem
-    {
-        public Guid Id { get; } = Guid.NewGuid();
-        public string Title { get; private set; }
-        public Boolean IsDone { get; private set; }
-
-        public TodoItem(string title) 
-        {
-            Title = title?.Trim() ?? throw new ArgumentNullException(nameof(title));
-        }
-        public void MarkDown() => IsDone = true;
-        public void MarkUndone() => IsDone = false;
-        public void Rename(string newTitle)
-        {
-            if (string.IsNullOrWhiteSpace(newTitle)) throw new ArgumentException("Title required", nameof(newTitle));
-            Title = newTitle.Trim();
-        }
-    }
-
     public class TodoList
     {
         private readonly List<TodoItem> _items = new();
